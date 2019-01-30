c99b3c107c7b757b180223cf40c43cc2a3f0fa7c
Vivien <vivien@gueant.org>
2019-01-30 11:33:18 +0100

    2 chiffres pour les N° de version mineurs

--- comp-speedtest.sh
+++ comp-speedtest.sh
@@ -3,7 +3,7 @@ set -o errexit
 # Comparateur de serveurs SpeedTest
 # https://lafibre.info/tester-son-debit/comp-speedtest/
 # Vivien GUEANT / LaFibre.info
-# Version 1.1
+# Version 1.01
 
 #### Dépendances à installer avant de lancer le script
 # sudo apt install speedtest-cli
